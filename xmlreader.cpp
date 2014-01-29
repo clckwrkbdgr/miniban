38df672497f7db1f35e235d093b9f6154e7b1417
Igor Chaika <kp580bm1@gmail.com>
2014-01-29 16:56:07 +0200

    Reading attributes for XMLReader.

--- xmlreader.cpp
+++ xmlreader.cpp
@@ -1,5 +1,11 @@
 #include "xmlreader.h"
+#include <chthon/files.h>
+#include <chthon/log.h>
+#include <chthon/format.h>
 #include <sstream>
+#include <cctype>
+using Chthon::format;
+using Chthon::log;
 
 XMLReader::XMLReader(std::istream & stream)
 	: s(stream)
@@ -20,9 +26,63 @@ const std::string & XMLReader::to_next_tag()
 		s >> ch;
 	}
 
+	enum { ERROR, TAG_NAME, ATTRIBUTE, EQUALS, VALUE };
+	std::string attribute;
+	int mode = TAG_NAME;
 	s >> ch;
 	while(ch != '>') {
-		current_tag += ch;
+		switch(mode) {
+			case TAG_NAME:
+				if(isspace(ch)) {
+					if(!current_tag.empty()) {
+						mode = ATTRIBUTE;
+						attribute.clear();
+					}
+				} else {
+					current_tag += ch;
+				}
+				break;
+			case ATTRIBUTE:
+				if(isspace(ch)) {
+					if(!attribute.empty()) {
+						mode = EQUALS;
+					}
+				} else {
+					if(ch == '=') {
+						mode = VALUE;
+					} else {
+						attribute += ch;
+					}
+				}
+				break;
+			case EQUALS:
+				if(!isspace(ch)) {
+					if(ch == '=') {
+						mode = VALUE;
+					} else {
+						mode = ERROR;
+					}
+				}
+				break;
+			case VALUE:
+				if(isspace(ch)) {
+					if(attributes.count(attribute) > 0) {
+						mode = ATTRIBUTE;
+						attribute.clear();
+					}
+				} else {
+					if(attributes.count(attribute) == 0 && ch == '"') {
+						s.seekg(-1, s.cur); // TODO replace with aware of that read_string routine.
+						attributes[attribute] = Chthon::read_string(s);
+						mode = ATTRIBUTE;
+						attribute.clear();
+					} else {
+						attributes[attribute] += ch;
+					}
+				}
+				break;
+			case ERROR: default: break;
+		}
 		s >> ch;
 	}
 	return current_tag;
