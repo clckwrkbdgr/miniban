5d5720762787a91f111f009ebda14056c859911e
Igor Chaika <kp580bm1@gmail.com>
2016-05-09 11:39:54 +0300

    Moved config file to XDG_DATA_DIR with fallback to XDG_CONFIG_DIR for backward compatibility sake.

--- src/settings.cpp
+++ src/settings.cpp
@@ -2,17 +2,43 @@
 #include <chthon2/files.h>
 #include <cstdlib>
 
+std::string get_xdg_config_dir()
+{
+	static const char * xdg_config_dir = getenv("XDG_CONFIG_DIR");
+	static std::string result = xdg_config_dir ? xdg_config_dir : "";
+	if(result.empty()) {
+		static const char * home_dir = getenv("HOME");
+		result = std::string(home_dir ? home_dir : "") + "/.config";
+	}
+	return result;
+}
+
+std::string get_xdg_data_dir()
+{
+	static const char * xdg_data_dir = getenv("XDG_DATA_DIR");
+	static std::string result = xdg_data_dir ? xdg_data_dir : "";
+	if(result.empty()) {
+		static const char * home_dir = getenv("HOME");
+		result = std::string(home_dir ? home_dir : "") + "/.local/share";
+	}
+	return result;
+}
+
 Settings::Settings()
 	: level_index(0)
 {
-	const char * home_dir = getenv("HOME");
-	filename = std::string(home_dir ? home_dir : "") + "/.config/miniban.config";
+	filename = get_xdg_data_dir() + "/miniban.save";
 }
 
 void Settings::load()
 {
+	static std::string obsolete_config_file = get_xdg_config_dir() + "/miniban.config";
 	if(Chthon::file_exists(filename)) {
 		std::ifstream file(filename, std::ifstream::in);
+		Chthon::Reader reader(file);
+		reader.store(level_index).store(levelset);
+	} else if(Chthon::file_exists(obsolete_config_file)) {
+		std::ifstream file(obsolete_config_file, std::ifstream::in);
 		Chthon::Reader reader(file);
 		reader.store(level_index).store(levelset);
 	}
